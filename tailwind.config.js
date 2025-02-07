fa5589a0bbb287c2b2f444e88af178d96a80fbaf
Illya <romanenkoisnot@gmail.com>
2025-02-08 01:28:47 +0200

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,32 @@ module.exports = {
   ],
   theme: {
     colors: {
+      ...require('tailwindcss/colors'),
       'primary': {
         DEFAULT: '#3375d8',
+      },
+      'dark': {
+        DEFAULT: '#0c0e14',
+      },
+      'dark-light': {
+        DEFAULT: '#10131b',
+      },
+      'dark-gray': {
+        DEFAULT: '#3a3a3a',
+      },
+      'gray': {
+        DEFAULT: '#9b9b9b',
       },
       'white': {
         DEFAULT: '#e6e6e6',
       },
-      // 'dark': {
-      //   DEFAULT: '#0c0e14',
-      // }
     },
-    extend: {},
   },
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
   ],
 }
+
+
 
