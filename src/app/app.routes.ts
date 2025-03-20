b524a8c16069c58376b4169f9199dd8e22060998
Romanenko Illya <romanenkoisnot@gmail.com>
2025-03-20 16:55:37 +0200

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,43 +3,43 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.component'),
+    loadComponent: () => import('@pages/home/home.component'),
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.component'),
+    loadComponent: () => import('@pages/login/login.component'),
   },
   {
     path: 'user',
-    loadComponent: () => import('./pages/user/user.component'),
+    loadComponent: () => import('@pages/user/user.component'),
   },
   {
     path: 'courses',
-    loadComponent: () => import('./pages/courses/courses.component'),
+    loadComponent: () => import('@pages/courses/courses.component'),
   },
   {
     path: 'course/:courseHref',
-    loadComponent: () => import('./pages/course/course.component'),
+    loadComponent: () => import('@pages/course/course.component'),
   },
   {
     path: 'course/:courseHref/:lessonHref',
-    loadComponent: () => import('./pages/lesson/lesson.component'),
+    loadComponent: () => import('@pages/lesson/lesson.component'),
   },
   {
     path: 'tests',
-    loadComponent: () => import('./pages/tests/tests.component'),
+    loadComponent: () => import('@pages/tests/tests.component'),
   },
   {
     path: 'test/:testCollectionHref/:testHref',
-    loadComponent: () => import('./pages/test/test.component'),
+    loadComponent: () => import('@pages/test/test.component'),
   },
   {
     path: 'console',
-    loadComponent: () => import('./pages/console/console.component'),
+    loadComponent: () => import('@pages/console/console.component'),
   },
   {
     path: 'error404',
-    loadComponent: () => import('./pages/error404/error404.component'),
+    loadComponent: () => import('@pages/error404/error404.component'),
     data: {title: 'Error 404'}
   },
   {
