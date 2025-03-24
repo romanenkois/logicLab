03bba226766452620c3a9cb7de6e70ec26cf06ca
Romanenko Illya <romanenkoisnot@gmail.com>
2025-03-25 00:44:12 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app/basic/app.config';
+import { AppComponent } from './app/basic/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
