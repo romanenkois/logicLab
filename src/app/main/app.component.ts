4cefe51d55726fe16c0985f91a25070f3fb5801d
Romanenko Illya <romanenkoisnot@gmail.com>
2025-03-30 18:21:50 +0300

    build: fix

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from "@widgets"
+import { NavBarComponent } from '@widgets';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, NavBarComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
