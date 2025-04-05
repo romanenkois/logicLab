2b29091260f345badea89f412a17f5c0c9853854
Romanenko Illya <romanenkoisnot@gmail.com>
2025-04-05 17:27:55 +0300

    fix

--- src/app/interface/features/lesson-block-code-sample/lesson-code-sample.component.ts
+++ src/app/interface/features/lesson-block-code-sample/lesson-code-sample.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { CodeSampleBlock } from '@types';
 import { Highlight } from 'ngx-highlightjs';
+import { ProgramingLanguagePipe } from '@pipes';
 
 @Component({
   selector: 'app-lesson-code-sample',
   standalone: true,
-  imports: [Highlight],
+  imports: [Highlight, ProgramingLanguagePipe],
   templateUrl: './lesson-code-sample.component.html',
   styleUrl: './lesson-code-sample.component.scss',
 })
