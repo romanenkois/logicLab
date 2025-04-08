d1d8c4a6b319e23d955c92f6e85d0e57cf2f25fd
Romanenko Illya <romanenkoisnot@gmail.com>
2025-04-08 20:10:13 +0300

    fix deploy

--- src/environments/environments.production.ts
+++ src/environments/environments.production.ts
@@ -1,6 +1,6 @@
 import { AppConfig } from '@types';
 
-export const config: AppConfig = {
+export const appConfig: AppConfig = {
   api: {
     BASE_API_URL: 'https://logic-lab-api-ts.vercel.app/v2',
     BASE_CLIENT_URL: 'https://logic-lab-two.vercel.app/',
