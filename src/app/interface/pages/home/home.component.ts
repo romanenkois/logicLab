0e36d7b805529c56dae9f7b6874cd1bc8b925e75
Romanenko Illya <romanenkoisnot@gmail.com>
2025-05-07 22:03:54 +0300

    fix

--- src/app/interface/pages/home/home.component.ts
+++ src/app/interface/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    imports: [],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'app-home',
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
-export default class HomeComponent {
-
-}
+export default class HomeComponent {}
